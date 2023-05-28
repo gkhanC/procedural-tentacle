6cdb2db7640c1f800d7042e8a981474b02c755e0
Gökhan Tutku Çay <59888233+gkhanC@users.noreply.github.com>
2023-05-28 16:45:08 +0300

    [Add:] Create TentacleLeg

---
+++ Assets/Scripts/procudural-tentacles/Creature/Tentacles/TentacleLeg.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace ProceduralTentacle.Creature.Tentacles
+{
+	[RequireComponent(typeof(LineRenderer))]
+	public class TentacleLeg : MonoBehaviour
+	{
+		private LineRenderer _lneRenderer;
+		private CreatureController _creatureController;
+		private TentacleLegData _data;
+
+		private Vector3[] parts;
+		private Vector3[] partOffsets;
+		public bool isInit { get; private set; }
+
+		public void Initialize(CreatureController creature, TentacleLegData data)
+		{
+			_lneRenderer = GetComponent<LineRenderer>();
+			_creatureController = creature;
+			_data = data;
+
+			parts = new Vector3[data.partCount];
+			partOffsets = new Vector3[data.partCount - 2];
+			PartOffsetInitial(ref partOffsets);
+
+			isInit = false;
+		}
+
+		public void PartOffsetInitial(ref Vector3[] partOffsets)
+		{
+			for (int i = 0; i < partOffsets.Length; i++)
+			{
+				partOffsets[i] = (Random.onUnitSphere *
+				                  Random.Range(_data.partOffset.min, _data.partOffset.max));
+			}
+		}
+	}
+}
--- Assets/Scripts/procudural-tentacles/Movement/MovementController.cs
+++ Assets/Scripts/procudural-tentacles/Movement/MovementController.cs
@@ -6,9 +6,6 @@ namespace ProceduralTentacle.Movement
 	[Serializable]
 	public abstract class MovementController
 	{
-		
-		
-		
 		public abstract void Move(Transform objectTransform, Vector3 direction, float speed, out Vector3 currentVelocity);
 	}
 }
