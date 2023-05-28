0ad2b5f2b690e7145ea412e9fb8481caad68c5c4
Gökhan Tutku Çay <59888233+gkhanC@users.noreply.github.com>
2023-05-28 16:44:41 +0300

    [Add:] Create TentacleLegData

---
+++ Assets/Scripts/procudural-tentacles/Creature/Tentacles/TentacleLegData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using HypeFire.Utilities.CustomStructures;
+using UnityEngine;
+
+namespace ProceduralTentacle.Creature.Tentacles
+{
+	[Serializable]
+	public struct TentacleLegData
+	{
+		public Vector3 footPosition;
+		public float resolution;
+		public float distance;
+		public float lerpMultiplier;
+		public float lifeTime;
+		public int partCount;
+		public MinMaxFloat partOffset;
+		public bool isInfinity;
+	}
+}
